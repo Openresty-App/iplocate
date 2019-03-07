ccc6e0b437083db08b84cf914a79c465d8cb2f06
zhangzheng <zzgigi2003@163.com>
2019-03-07 17:59:00 +0800

    update t.sh

--- t.sh
+++ t.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
-curl http://127.0.0.1:10905/query?ip=8.8.8.8
+curl http://127.0.0.1:10905/query?ip=117.136.19.28
 echo ""
 
+curl http://127.0.0.1:10905/query?ip=8.8.8.8
+echo ""
 
 curl http://127.0.0.1:10905/query?ip=202.106.0.20
 echo ""

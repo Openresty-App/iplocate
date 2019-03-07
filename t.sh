fb24079f45753b1693cdb5aa28a4484db080a99a
zhangzheng <zzgigi2003@163.com>
2019-03-07 17:55:20 +0800

    add t.sh

---
+++ t.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+curl http://127.0.0.1:10905/query?ip=8.8.8.8
+echo ""
+
+
+curl http://127.0.0.1:10905/query?ip=202.106.0.20
+echo ""
+
+curl http://127.0.0.1:10905/query?ip=192.168.1.1
+echo ""
